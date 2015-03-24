2aacf2e510c49b41eeaedc41db7fb8da2fa2f73a
Kozlov Fedor <kozlov@eis.ru>
2015-03-24 18:59:33 +0300

    add init commit for pdf parser

---
+++ ceur-ws-pdfs/CeurWsPDFParser/namespaces.py
@@ -0,0 +1,9 @@
+from rdflib import Namespace
+
+SWRC = Namespace("http://swrc.ontoware.org/ontology#")
+DBPEDIAOWL = Namespace("http://dbpedia.org/ontology/")
+SWC = Namespace("http://data.semanticweb.org/ns/swc/ontology#")
+EVENT = Namespace("http://purl.org/NET/c4dm/event.owl#")
+TIMELINE = Namespace("http://purl.org/NET/c4dm/timeline.owl#")
+BIBO = Namespace("http://purl.org/ontology/bibo/")
+SKOS = Namespace("http://www.w3.org/2004/02/skos/core#")
