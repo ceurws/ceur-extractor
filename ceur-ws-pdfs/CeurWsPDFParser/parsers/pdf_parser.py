2245d665c4b97bf26735cf2d395780203c4b0f30
Kozlov Fedor <kozlovfedor@gmail.com>
2015-04-10 15:49:53 +0300

    [+] mappings for funds and ontologies

--- ceur-ws-pdfs/CeurWsPDFParser/parsers/pdf_parser.py
+++ ceur-ws-pdfs/CeurWsPDFParser/parsers/pdf_parser.py
@@ -15,8 +15,8 @@ from rdflib.plugins.stores.sparqlstore import SPARQLStore
 import config
 from base import Parser, find_university_in_dbpedia, create_proceedings_uri, create_publication_uri, clean_string
 from pdf_parser_lib_mock import PDFParserLibMock
-from namespaces import SWRC, DBPEDIAOWL, BIBO 
-from rdflib.namespace import RDF, RDFS, FOAF, DCTERMS, DC, XSD
+from namespaces import SWRC, DBPEDIAOWL, BIBO, ARPFO 
+from rdflib.namespace import RDF, RDFS, FOAF, DCTERMS, DC, XSD, OWL
 
 import pdf_extraction_lib as pdf_convert
 from metadata_extraction_lib import PDFmetadataExtractionLib
@@ -69,6 +69,7 @@ class PDFParser(Parser):
         for cited_work in self.data['cited_works']:
                 cw = URIRef(config.id['cited_work'] + urllib.quote(cited_work["title"].encode('utf-8')))
                 triples.append((cw, RDF.type, BIBO.Document))
+                triples.append((resource, BIBO.cites, cw))                
                 triples.append((cw, BIBO.title, Literal(cited_work["title"], datatype=XSD.string)))
                 if "doi" in cited_work:
                     triples.append((cw, BIBO.doi, Literal(cited_work["doi"], datatype=XSD.string)))
@@ -82,7 +83,41 @@ class PDFParser(Parser):
             
                     
         # grants and projects
+        for grant in self.data['grants']:
+            grt = URIRef(config.id['grant'] + urllib.quote(grant["title"].encode('utf-8')))
+            triples.append((grt, RDF.type, ARPFO.Grant))
+            triples.append((grt, ARPFO.funds, resource))
+            triples.append((grt, BIBO.name, Literal(grant["title"], datatype=XSD.string)))
+            if "id" in grant:
+                triples.append((grt, ARPFO.grantNumber, Literal(grant["id"], datatype=XSD.string)))
+
+        for agency in self.data['fundings']:
+            agnc = URIRef(config.id['agency'] + urllib.quote(agency.encode('utf-8')))
+            triples.append((agnc, RDF.type, FOAF.Organization))
+            triples.append((agnc, ARPFO.funds, resource))
+            triples.append((agnc, BIBO.name, Literal(agency, datatype=XSD.string)))
+
+        for project in self.data['eu_projects']:
+            prj = URIRef(config.id['project'] + urllib.quote(project.encode('utf-8')))
+            triples.append((prj, RDF.type, ARPFO.Project))
+            triples.append((prj, ARPFO.funds, resource))
+            triples.append((prj, BIBO.name, Literal(project, datatype=XSD.string)))
+
+        
         # ontologies
+        for ontology in self.data['new_ontologies']:
+            ont = URIRef(config.id['ontology'] + urllib.quote(ontology.encode('utf-8')))
+            triples.append((ont, RDF.type, OWL.Ontology))
+            triples.append((resource, BIBO.presents, ont))
+            triples.append((ont, RDFS.label, Literal(ontology, datatype=XSD.string)))
+
+        for ontology in self.data['related_ontologies']:
+            ont = URIRef(config.id['ontology'] + urllib.quote(ontology.encode('utf-8')))
+            triples.append((ont, RDF.type, OWL.Ontology))
+            triples.append((resource, BIBO.reviewOf, ont))
+            triples.append((ont, RDFS.label, Literal(ontology, datatype=XSD.string)))
+
+
         self.write_triples(triples)
 
     def parse_template_1(self):
